b47e918dfc095a75c9f5d434811b9ca339d05623
Edwin5658 <edwinchiu618@gmail.com>
2026-01-11 12:07:07 +0800

    updated script

--- urlencode.bat
+++ urlencode.bat
@@ -1,9 +1,2 @@
 @echo off
-REM Check if there are arguments passed (e.g., urlencode "my text")
-if "%~1"=="" (
-    REM No arguments? Read from the Pipe (e.g., echo "hello" | urlencode)
-    powershell -NoProfile -Command "$input | ForEach-Object { Write-Host ([uri]::EscapeDataString($_)) }"
-) else (
-    REM Arguments found? Encode the arguments directly
-    powershell -NoProfile -Command "Write-Host ([uri]::EscapeDataString('%*'))"
-)
+powershell -NoProfile -Command "$text = Read-Host 'Enter text to encode'; Write-Host ([uri]::EscapeDataString($text))"
